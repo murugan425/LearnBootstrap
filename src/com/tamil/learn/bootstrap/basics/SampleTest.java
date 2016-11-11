1f1b2cdf31df2317d76fc972fa18e31371f739a0
Murugan_Nagarajan <Murugan_Nagarajan@CHNMCT105003L.ad.infosys.com>
2016-11-12 00:55:43 +0800

    Sample java class for testing java snippets

---
+++ src/com/tamil/learn/bootstrap/basics/SampleTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.tamil.learn.bootstrap.basics;
+
+/**
+ * @author Murugan_Nagarajan
+ *
+ */
+public class SampleTest {
+
+	/**
+	 * 
+	 */
+	public SampleTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
